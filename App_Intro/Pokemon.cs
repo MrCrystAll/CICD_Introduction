2a88165f5317b376b1042bb9d5a9ba4a615f8d5f
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2023-05-19 19:18:49 +0200

    Quite a big commit, adding the images/thumbnails/sprites, details page is ready, ui has to be worked on

--- App_Intro/Pokemon.cs
+++ App_Intro/Pokemon.cs
@@ -1,9 +1,34 @@
 ﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace App_Intro;
 
 public class Pokemon
 {
+    private int _id;
+
+    [JsonPropertyName("id")]
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            Image = "data/images/" + _id.ToString("000") + ".png";
+            Thumbnail = "data/thumbnails/" + _id.ToString("000") + ".png";
+            Sprite = "data/sprites/" + _id.ToString("000") + "MS.png";
+        }
+    }
+    public string Image { get; set; }
+    public string Thumbnail { get; set; }
+    public string Sprite { get; set; }
+    
     [JsonPropertyName("name")]
     public Name Name { get; set; }
+    
+    [JsonPropertyName("base")]
+    public Stats BaseStats { get; set; }
+
+    [JsonPropertyName("type")]
+    public string[] Types { get; set; }
 }
---
+++ App_Intro/Stats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace App_Intro;
+
+public class Stats
+{
+    [JsonPropertyName("HP")]
+    public float HP { get; set; }
+    
+    [JsonPropertyName("Attack")]
+    public float Attack { get; set; }
+    
+    [JsonPropertyName("Defense")]
+    public float Defense { get; set; }
+    
+    [JsonPropertyName("Sp. Attack")]
+    public float SpAttack { get; set; }
+    
+    [JsonPropertyName("Sp. Defense")]
+    public float SpDefense { get; set; }
+    
+    [JsonPropertyName("Speed")]
+    public float Speed { get; set; }
+
+    private float OtherThanHPStatAt(float stat, int level) => (float)Math.Floor(0.01 * (2 * stat * level) + 5);
+
+    public Stats CalculateStatsAt(int level)
+    {
+        return new Stats
+        {
+            HP = (float)Math.Floor(0.01 * (2 * HP * level) + level + 10),
+            Attack = OtherThanHPStatAt(Attack, level),
+            Defense = OtherThanHPStatAt(Defense, level),
+            SpAttack = OtherThanHPStatAt(SpAttack, level),
+            SpDefense = OtherThanHPStatAt(SpDefense, level),
+            Speed = OtherThanHPStatAt(Speed, level)
+        };
+    }
+}
