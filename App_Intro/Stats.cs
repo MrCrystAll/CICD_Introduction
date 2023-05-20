8b57a82060283d4c91573eaab1a69917d61069f7
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2023-05-20 15:29:08 +0200

    Added stats calculation based on ivs, evs and level

--- App_Intro/Stats.cs
+++ App_Intro/Stats.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 
 namespace App_Intro;
 
-public class Stats
+public class Stats : INotifyPropertyChanged
 {
+    private uint _level;
+    public uint Level
+    {
+        get => _level;
+        set
+        {
+            _level = value;
+            OnPropertyChanged();
+        }
+    }
+    
     [JsonPropertyName("HP")]
     public float HP { get; set; }
     
@@ -23,18 +39,36 @@ public class Stats
     [JsonPropertyName("Speed")]
     public float Speed { get; set; }
 
-    private float OtherThanHPStatAt(float stat, int level) => (float)Math.Floor(0.01 * (2 * stat * level) + 5);
+    public ObservableCollection<int> EVs { get; set; } = new(){0, 0, 0, 0, 0, 0};
+    public ObservableCollection<int> IVs { get; set; } = new(){0, 0, 0, 0, 0, 0};
 
-    public Stats CalculateStatsAt(int level)
+    private float OtherThanHPStatAt(float stat, int ev, int iv) => (float)Math.Floor(0.01 * (2 * stat  + iv + Math.Floor(ev * 0.25)) * Level) + 5;
+
+    public Stats CalculateStats()
     {
         return new Stats
         {
-            HP = (float)Math.Floor(0.01 * (2 * HP * level) + level + 10),
-            Attack = OtherThanHPStatAt(Attack, level),
-            Defense = OtherThanHPStatAt(Defense, level),
-            SpAttack = OtherThanHPStatAt(SpAttack, level),
-            SpDefense = OtherThanHPStatAt(SpDefense, level),
-            Speed = OtherThanHPStatAt(Speed, level)
+            HP = (float)Math.Floor(0.01 * (2 * HP + IVs[0] + Math.Floor(EVs[0] * 0.25)) * Level) + Level + 10,
+            Attack = OtherThanHPStatAt(Attack, EVs[1], IVs[1]),
+            Defense = OtherThanHPStatAt(Defense, EVs[2], IVs[2]),
+            SpAttack = OtherThanHPStatAt(SpAttack, EVs[3], IVs[3]),
+            SpDefense = OtherThanHPStatAt(SpDefense, EVs[4], IVs[4]),
+            Speed = OtherThanHPStatAt(Speed, EVs[5], IVs[5])
         };
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
---
+++ App_Intro/StatsDisplayer.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace App_Intro;
+
+public partial class StatsDisplayer
+{
+
+    public static readonly DependencyProperty StatsProperty = DependencyProperty.Register(
+        nameof(Stats), typeof(Stats), typeof(StatsDisplayer), new PropertyMetadata(default(Stats)));
+    public Stats Stats
+    {
+        get => (Stats)GetValue(StatsProperty);
+        set => SetValue(StatsProperty, value);
+    }
+    
+    public StatsDisplayer()
+    {
+        InitializeComponent();
+    }
+}
