126419ca74efe71e0377692f064689dcbfe701be
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2023-05-29 16:19:09 +0200

    Added direct access to previous/next evolutions Pokemon

--- App_Intro/model/EvolutionProfile.cs
+++ App_Intro/model/EvolutionProfile.cs
@@ -1,12 +1,31 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Net.Mime;
+using System.Text.Json.Serialization;
+using System.Windows;
+using App_Intro.views;
 
 namespace App_Intro.model;
 
 public class EvolutionProfile
 {
     [JsonPropertyName("prev")]
-    public string[] Previous { get; set; }
+    private string[] _previous { get; set; }
     
     [JsonPropertyName("next")]
-    public string[][] Next { get; set; }
+    private string[][] _next { get; set; }
+
+    public Pokemon PreviousPokemon => (Application.Current as App).Manager.Pokemons[int.Parse(_previous[0]) - 1];
+
+    private Pokemon[] _getNextPokemons()
+    {
+        var temp = new Pokemon[_next.Length];
+
+        for (int i = 0; i < _next.Length; i++)
+        {
+            temp[i] = (Application.Current as App).Manager.Pokemons[int.Parse(_next[i][0]) - 1];
+        }
+
+        return temp;
+    }
+
+    public Pokemon[] NextPokemons => _getNextPokemons();
 }
