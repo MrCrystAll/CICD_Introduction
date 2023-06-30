94833e18fab77611a26a0628f4cece071cf8684f
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2023-06-30 10:30:48 +0200

    changed the display of evolutions to something else

--- App_Intro/DataParser.cs
+++ App_Intro/DataParser.cs
@@ -1,11 +1,14 @@
 ﻿using System.IO;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 using App_Intro.model;
 
 namespace App_Intro;
 
 public static class DataParser
 {
+    private static HttpClient _client = new HttpClient();
     public static Pokemon[] LoadPokemon()
     {
         using StreamReader reader = new StreamReader("data/pokedex.json");
@@ -14,5 +17,14 @@ public static class DataParser
         var obj = JsonSerializer.Deserialize<Pokemon[]>(json);
 
         return obj;
+    }
+
+    public static async Task<Pokemon?> GetPokemon(string id)
+    {
+        var response = await _client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + id);
+        Stream stream = response.Content.ReadAsStream();
+        using StreamReader sr = new StreamReader(stream);
+
+        return JsonSerializer.Deserialize<Pokemon>(await sr.ReadToEndAsync());
     }
 }
---
+++ App_Intro/views/OnePokemonLabel.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using App_Intro.model;
+
+namespace App_Intro.views;
+
+public partial class OnePokemonLabel : UserControl
+{
+
+    public static readonly DependencyProperty PokemonProperty = DependencyProperty.Register(
+        nameof(Pokemon), typeof(Pokemon), typeof(OnePokemonLabel), new PropertyMetadata(default(Pokemon)));
+
+    public Pokemon Pokemon
+    {
+        get { return (Pokemon)GetValue(PokemonProperty); }
+        set { SetValue(PokemonProperty, value); }
+    }
+    
+    public OnePokemonLabel()
+    {
+        InitializeComponent();
+    }
+}
